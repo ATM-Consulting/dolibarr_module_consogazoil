d03a7908a8919569a75e288165eeff8d71ff9726
florian HENRY <florian.henry@atm-consulting.fr>
2017-06-23 09:45:59 +0200

    move lib file to includes

---
+++ lib/include_datatables.php
@@ -0,0 +1,37 @@
+<?php
+/* Copyright (C) 2012-2013	Regis Houssin	<regis.houssin@capnetworks.com>
+ * Copyright (C) 2013-2017	Florian HENRY 		<florian.henry@atm-consulting.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+$TJs= array('/consogazoil/includes/datatables/media/js/jquery.dataTables.min.js',
+'/consogazoil/includes/datatables/extensions/Buttons/js/dataTables.buttons.min.js',
+'/consogazoil/includes/datatables/extensions/Buttons/js/buttons.colVis.min.js',
+'/consogazoil/includes/datatables/extensions/Buttons/js/buttons.html5.min.js',
+'/consogazoil/includes/datatables/extensions/Buttons/js/buttons.print.min.js',
+'/consogazoil/includes/datatables/extensions/ColReorder/js/dataTables.colReorder.min.js',
+'/consogazoil/includes/jstz/jszip.min.js',
+'/consogazoil/includes/pdfmake/pdfmake.min.js',
+'/consogazoil/includes/vfs_fonts/vfs_fonts.js');
+
+$TCss = array(
+		'/consogazoil/includes/datatables/media/css/jquery.dataTables.min.css',
+		'/consogazoil/includes/datatables/extensions/Buttons/css/buttons.dataTables.min.css',
+		'/consogazoil/includes/datatables/extensions/ColReorder/css/colReorder.dataTables.min.css',
+);
+
+?>
